1506346129408ed5f9566cba7f29e00420fe03fb
thoppe <travis.hoppe@gmail.com>
2015-04-10 00:39:16 -0400

    Added local server code

---
+++ src/http_server.py
@@ -0,0 +1,44 @@
+# helper to load url
+# runs webserver and loads url with webbrowswer module
+
+import SimpleHTTPServer, BaseHTTPServer
+import socket
+import thread
+import webbrowser
+
+
+def load_url(path):
+    PORT = 8000
+    httpd = StoppableHTTPServer(("127.0.0.1",PORT), handler)
+    thread.start_new_thread(httpd.serve, ())
+    webbrowser.open_new('http://localhost:%s/%s'%(PORT,path))
+    input("Press <RETURN> to stop server\n")
+    httpd.stop()
+    print("To restart server run: \n%s"%server)
+
+class StoppableHTTPServer(BaseHTTPServer.HTTPServer):
+
+    def server_bind(self):
+        BaseHTTPServer.HTTPServer.server_bind(self)
+        self.socket.settimeout(1)
+        self.run = True
+
+    def get_request(self):
+        while self.run:
+            try:
+                sock, addr = self.socket.accept()
+                sock.settimeout(None)
+                return (sock, addr)
+            except socket.timeout:
+                pass
+
+    def stop(self):
+        self.run = False
+
+    def serve(self):
+        while self.run:
+            self.handle_request()
+
+handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+input = raw_input
+server = "python -m SimpleHTTPServer 8000"
